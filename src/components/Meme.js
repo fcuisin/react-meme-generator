94a8fa179b948dd2b5e9ebf8160977ca8a9ff253
Ferdinand Cuisin <fcuisin@gmail.com>
2021-03-31 21:27:20 +0200

    Display selected img in the left container

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -4,6 +4,15 @@ function Meme({img, textTop, textBottom}) {
 
     return(
       <div className="App-meme-window">
+        <svg
+        height="600px"
+        viewBox="0 0 579 375"
+        preserveAspectRatio="xMidYMid meet"
+        >
+          <image xlinkHref={img.url} x="0" y="0" width="100%" height="100%"></image>
+          <text>{textTop}</text>
+          <text>{textBottom}</text>
+        </svg>
 
       </div>
     )
