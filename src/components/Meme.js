75428337ff027ac26cfb2aa25370c6c06f6def3b
Ferdinand Cuisin <fcuisin@gmail.com>
2021-04-02 10:59:02 +0200

    Set up drag and drop logic

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,12 +1,34 @@
-import React, {useRef} from 'react';
+import React, {useState, useRef} from 'react';
 import {saveSvgAsPng} from 'save-svg-as-png';
 
 function Meme({img, textTop, textBottom}) {
 
     const ref = useRef();
+    const imageRef = useRef();
+
+    const [positionTop, setPositionTop] = useState({x: 100, y :200})
 
     const downloadMeme = () => {
       saveSvgAsPng(ref.current, `Meme Generator - ${img.name}.png`)
+    }
+
+    const getTextPosition = (e) => {
+
+      let rect = imageRef.current.getBoundingClientRect();
+
+      const xOffset = e.clientX - rect.left;
+      const yOffset = e.clientY - rect.top;
+
+      setPositionTop({x:xOffset, y:yOffset});
+    }
+
+    const handleMouseDown = (e) => {
+      console.log("Down")
+      document.addEventListener('mousemove', getTextPosition);
+      document.addEventListener('mouseup', (e) => {
+        console.log('Up')
+        document.removeEventListener('mousemove', getTextPosition)
+      })
     }
 
     return(
@@ -18,8 +40,8 @@ function Meme({img, textTop, textBottom}) {
         viewBox="0 0 579 375"
         preserveAspectRatio="xMidYMid meet"
         >
-          <image xlinkHref={img.url} x="0" y="0" width="100%" height="100%"></image>
-          <text>{textTop}</text>
+          <image ref={imageRef} xlinkHref={img.url} x="0" y="0" width="100%" height="100%"></image>
+          <text  x={positionTop.x} y={positionTop.y} onMouseDown={event => handleMouseDown(event)}>{textTop}</text>
           <text>{textBottom}</text>
         </svg>
 
