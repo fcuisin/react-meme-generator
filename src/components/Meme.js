ea24e0cc37d04ad5a8474e9b9c374bfb04363e18
Ferdinand Cuisin <fcuisin@gmail.com>
2021-04-02 11:42:02 +0200

    Handle drag and drop for top and bottom text

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -6,29 +6,58 @@ function Meme({img, textTop, textBottom}) {
     const ref = useRef();
     const imageRef = useRef();
 
-    const [positionTop, setPositionTop] = useState({x: 100, y :200})
+    const [positionTop, setPositionTop] = useState({x: 250, y :50})
+    const [positionBottom, setPositionBottom] = useState({x: 250, y :350})
 
     const downloadMeme = () => {
       saveSvgAsPng(ref.current, `Meme Generator - ${img.name}.png`)
     }
 
-    const getTextPosition = (e) => {
+    const textStyles = {
+      cursor: 'pointer',
+      fontSize: '36px',
+      textTransform: 'uppercase',
+      fontFamily: 'sans-serif',
+      fontWeight: 'bold'
+    }
+
+    const getTextPositionTop = (e, type) => {
 
       let rect = imageRef.current.getBoundingClientRect();
 
-      const xOffset = e.clientX - rect.left;
+      const xOffset = e.clientX - rect.left - 70;
       const yOffset = e.clientY - rect.top;
 
       setPositionTop({x:xOffset, y:yOffset});
+
     }
 
-    const handleMouseDown = (e) => {
-      console.log("Down")
-      document.addEventListener('mousemove', getTextPosition);
-      document.addEventListener('mouseup', (e) => {
-        console.log('Up')
-        document.removeEventListener('mousemove', getTextPosition)
-      })
+    const getTextPositionBottom = (e, type) => {
+
+      let rect = imageRef.current.getBoundingClientRect();
+
+      const xOffset = e.clientX - rect.left - 60;
+      const yOffset = e.clientY - rect.top;
+
+      setPositionBottom({x:xOffset, y:yOffset});
+
+    }
+
+    const handleMouseDown = (e, type) => {
+
+      if (type === "top") {
+        document.addEventListener('mousemove', getTextPositionTop);
+        document.addEventListener('mouseup', (e) => {
+          document.removeEventListener('mousemove', getTextPositionTop)
+        })
+      }
+
+      if (type === "bottom") {
+        document.addEventListener('mousemove', getTextPositionBottom);
+        document.addEventListener('mouseup', (e) => {
+          document.removeEventListener('mousemove', getTextPositionBottom)
+        })
+      }
     }
 
     return(
@@ -41,8 +70,8 @@ function Meme({img, textTop, textBottom}) {
         preserveAspectRatio="xMidYMid meet"
         >
           <image ref={imageRef} xlinkHref={img.url} x="0" y="0" width="100%" height="100%"></image>
-          <text  x={positionTop.x} y={positionTop.y} onMouseDown={event => handleMouseDown(event)}>{textTop}</text>
-          <text>{textBottom}</text>
+          <text x={positionTop.x} y={positionTop.y} onMouseDown={event => handleMouseDown(event, "top")}  style={{...textStyles}}>{textTop}</text>
+          <text x={positionBottom.x} y={positionBottom.y} onMouseDown={event => handleMouseDown(event, "bottom")}  style={{...textStyles}}>{textBottom}</text>
         </svg>
 
       </div>
